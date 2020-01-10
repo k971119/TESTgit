013831453f8e9b97cda76085c00d95ac8ed4f10c
sys-Normal <sys36203@gmail.com>
2020-01-10 18:04:56 +0900

    200110 save

--- project_1/js/function.js
+++ project_1/js/function.js
@@ -1,4 +1,17 @@
 $(function(){
     console.log("js conect")
 
+    $main_home = $("header");
+
+    $main_home.find("mnuOpen").on("mouseenter", function(){
+        $main_home.find(".bar").css({
+            width : 100 + "%"
+        });
+    });
+
+    // $main_home.find(".link").on("mouseenter", function(){
+    //     $main_home.find(".link").stop().animate()({
+    //         background : "linear-gradient(to right, #fff , #00aaad)"
+    //     });
+    // });
 });
